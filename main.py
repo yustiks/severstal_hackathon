d8dd04399547866e4092e2fc44a734b2b6dfcbfe
yustina.ivanova <yustina.ivanova@texel.graphics>
2019-02-23 17:06:48 +0300

    added multiple objects

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def main(args):
     input_objects = args.input_objects
     input_background = args.input_background
     output_folder = args.output_folder
+
     names_background = []
     background_len = 0
     for filename in os.listdir(input_background):
@@ -23,38 +24,46 @@ def main(args):
 
     # iterate through the folder with objects
     num_img = 0
-    for filename in os.listdir(input_objects):
-        object_img = cv2.imread(input_objects + '/' + filename)
-        id_back = random.randint(1, background_len)
-        background_img = cv2.imread(input_background + '/' + back_images.loc[id_back, 'name_file'])
-        # post the image randomly into this background image
-        back_height, back_width, _ = background_img.shape
-        object_height, object_width, _ = object_img.shape
-        min_x = object_width // 2 + 1
-        max_x = back_width - (object_width // 2 + 1)
-        min_y = object_height // 2 + 1
-        max_y = back_height - (object_height // 2 + 1)
-        x_center = random.randint(min_x, max_x)
-        y_center = random.randint(min_y, max_y)
-        x_start = x_center - object_width // 2
-        y_start = y_center - object_height // 2
-        background_img[y_start:y_start + object_height, x_start:x_start + object_width] = object_img
-        cv2.imwrite(output_folder + '/' + str(num_img) + '.jpg', background_img)
-        # save text files to the same folder with the same name
-        x_yolo = x_center/back_width
-        y_yolo = y_center/back_height
-        width_yolo = object_width/back_width
-        height_yolo = object_height/back_height
-        class_id = 0
-        output_line = str(class_id) + ' ' + str(x_yolo) + ' ' + str(y_yolo) + ' ' + str(width_yolo) + ' ' + str(height_yolo)
-        f = open(output_folder + '/' + str(num_img) + '.txt', "w+")
-        f.write(output_line)
-        f.close()
+    # iterate through different classes of objects
+    print(len(input_objects))
+    print(input_objects)
+    for i in range(len(input_objects)):
+        for filename in os.listdir(input_objects[i]):
+            object_img = cv2.imread(input_objects[i] + '/' + filename)
+            id_back = random.randint(1, background_len)
+            background_img = cv2.imread(input_background + '/' + back_images.loc[id_back, 'name_file'])
+            # post the image randomly into this background image
+            back_height, back_width, _ = background_img.shape
+            object_height, object_width, _ = object_img.shape
+            min_x = object_width // 2 + 1
+            max_x = back_width - (object_width // 2 + 1)
+            min_y = object_height // 2 + 1
+            max_y = back_height - (object_height // 2 + 1)
+            x_center = random.randint(min_x, max_x)
+            y_center = random.randint(min_y, max_y)
+            x_start = x_center - object_width // 2
+            y_start = y_center - object_height // 2
+            background_img[y_start:y_start + object_height, x_start:x_start + object_width] = object_img
+            cv2.imwrite(output_folder + '/' + str(num_img) + '.jpg', background_img)
+            # save text files to the same folder with the same name
+            x_yolo = x_center / back_width
+            y_yolo = y_center / back_height
+            width_yolo = object_width / back_width
+            height_yolo = object_height / back_height
+            class_id = 0
+            output_line = str(class_id) + ' ' + str(x_yolo) + ' ' + str(y_yolo) + ' ' + str(width_yolo) + ' ' + str(
+                height_yolo)
+            f = open(output_folder + '/' + str(num_img) + '.txt', "w+")
+            f.write(output_line)
+            f.close()
+            num_img += 1
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('input_objects', type=str, default='./input_objects', help='')
-    parser.add_argument('input_background', type=str, default='./input_background', help='')
-    parser.add_argument('output_folder', type=str, default='./output_folder', help='')
+    parser.add_argument('--input_objects', nargs='+', type=str, default='./input_objects_1 ./input_objects_2', help='')
+    parser.add_argument('--input_background', type=str, default='./input_background', help='')
+    parser.add_argument('--output_folder', type=str, default='./output_folder', help='')
+
     args = parser.parse_args()
     main(args)
