df6fe7317e88db480f58203b48b7f73c4c6bf885
yustina.ivanova <yustina.ivanova@texel.graphics>
2019-02-24 15:24:10 +0300

    matching

--- matching.py
+++ matching.py
@@ -37,25 +37,30 @@ def generate_patches(img1, img2):
             x2 = x1 + step
             sum1 = int(np.sum(img1[y1:y2, x1:x2]))
             sum2 = int(np.sum(img2[y1:y2, x1:x2]))
-            if abs(sum1+sum2) > sum_average:
-                list_of_predicted_patches.append(x1, y1)
+            if abs(sum1-sum2) > 2 * sum_average:
+                list_of_predicted_patches.append([x1, y1])
     return list_of_predicted_patches
 
 
-def draw_patches(list_of_patches):
+def draw_patches(list_of_patches, img2):
     for i in range(len(list_of_patches)):
-
+        x1 = list_of_patches[i][0]
+        y1 = list_of_patches[i][1]
+        x2 = x1 + step
+        y2 = y1 + step
+        cv2.rectangle(img2,(x1,y1),(x2,y2),(0,255,0),2)
+    cv2.imwrite('output.jpg', img2)
 
 def main(args):
     file1 = args.file1
     file2 = args.file2
-    img1 = cv2.imread(file1, 0)
-    img2 = cv2.imread(file2, 0)
-    img1 = normalize(img1)
-    img2 = normalize(img2)
+    img1_ = cv2.imread(file1, 0)
+    img2_ = cv2.imread(file2, 0)
+    img1 = normalize(img1_)
+    img2 = normalize(img2_)
 
     list_of_patches = generate_patches(img1, img2)
-    draw_patches(list_of_patches)
+    draw_patches(list_of_patches, img2_)
     return 0
 
 if __name__ == '__main__':
